1dd1fbd3f50b0772400a59adde140994c5e10bdc
Fabrizio Coccetti <fabrizio.coccetti@cref.it>
2024-11-22 10:38:21 +0100

    added logging

--- cnn-phase5resizeloop.py
+++ cnn-phase5resizeloop.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 import os, sys
 import cv2
 import time
+import logging
 from keras import datasets, layers, models
 from sklearn.metrics import classification_report
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from keras.losses import SparseCategoricalCrossentropy
+
+# Set up logging configuration
+logging.basicConfig(filename='cnn_phase5resizeloop.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 tf.random.set_seed(1234)  # initialize random seed for tensorflow
 
@@ -15,8 +19,8 @@ x_resize = 128  # width
 y_resize = 128  # high
 
 # Data path
-data_path = r'/Volumes/EXTERNAL_US/2024_06_12/'
-# data_path = r'data3'
+# data_path = r'/Volumes/EXTERNAL_US/2024_06_12/'
+data_path = r'data3'
 
 # Date of measurements
 date=''
@@ -34,28 +38,28 @@ classes=["input_mask_blank_screen",
          "input_mask_checkboard_4A", "input_mask_checkboard_4B"]
 
 # Loop over different nRUN values
-for nRUN in range(30, 301, 10):
-    print(f"\nRunning with nRUN = {nRUN}")
+for nRUN in range(30, 31, 10):
+    logging.info(f"\nRunning with nRUN = {nRUN}")
 
     # Number of frames
     frame_number = nRUN * np.size(classes)
 
-    # Print values
-    print("\n=================================================================")
-    print("Input values: ")
-    print(" - nRUN: ", nRUN)
-    print(" - Classes in words:", classes)
-    print(" - Classes in numbers: ", np.arange(np.size(classes)))
-    print(" - Total number of classes: ", np.size(classes))
-    print(" - Measured data | frame_number (number of RUNs * number of classes): ", frame_number)
-    print("=================================================================")
+    # Log values
+    logging.info("\n=================================================================")
+    logging.info("Input values: ")
+    logging.info(f" - nRUN: {nRUN}")
+    logging.info(f" - Classes in words: {classes}")
+    logging.info(f" - Classes in numbers: {np.arange(np.size(classes))}")
+    logging.info(f" - Total number of classes: {np.size(classes)}")
+    logging.info(f" - Measured data | frame_number (number of RUNs * number of classes): {frame_number}")
+    logging.info("=================================================================")
 
     # Determine high and width of the matrix reading the data
-    print("\n### Start of reading data ###")
+    logging.info("\n### Start of reading data ###")
     sample_measured_phase_full_path = os.path.join(data_path, date, data_type_measure, "M00001", classes[0], "files", "measured_phase.npy")
     sample_measured_phase = np.load(sample_measured_phase_full_path)
     high, width = sample_measured_phase.shape
-    print("\nLoaded sample phase with dimensions in px (high, width): ", high, width)
+    logging.info(f"\nLoaded sample phase with dimensions in px (high, width): {high}, {width}")
 
     # Load data and plot
     measured_phase = np.zeros((frame_number, high, width))
@@ -76,12 +80,12 @@ for nRUN in range(30, 301, 10):
             k += 1
             j += 1
 
-    # Print arrays dimensions
-    print("- measured_phase tensor shape: ", tf.shape(measured_phase).numpy())
-    print("- reference_class_short tensor shape: ", reference_class_short.shape)
+    # Log arrays dimensions
+    logging.info(f"- measured_phase tensor shape: {tf.shape(measured_phase).numpy()}")
+    logging.info(f"- reference_class_short tensor shape: {reference_class_short.shape}")
 
     # Resize images
-    print("\n### Start of resizing ###")
+    logging.info("\n### Start of resizing ###")
     resize_measured_phase = np.zeros((frame_number, y_resize, x_resize))
     for ii in range(frame_number):
         resize_measured_phase[ii] = cv2.resize(measured_phase[ii], (y_resize, x_resize), interpolation=cv2.INTER_NEAREST)
@@ -89,13 +93,13 @@ for nRUN in range(30, 301, 10):
 
     # Split data between Train and Test, then Normalization
     train_set_percentage = 0.5  # Set training set percentage
-    print("\n### Splitting data ###")
+    logging.info("\n### Splitting data ###")
     train_measured_phase, test_measured_phase = tf.split(measured_phase, [np.int32(frame_number * train_set_percentage), frame_number - np.int32(frame_number * train_set_percentage)])
     train_reference_class_short, test_reference_class_short = tf.split(reference_class_short, [np.int32(frame_number * train_set_percentage), frame_number - np.int32(frame_number * train_set_percentage)])
-    print("- train_measured_phase tensor shape: ", tf.shape(train_measured_phase).numpy())
-    print("- test_measured_phase tensor shape: ", tf.shape(test_measured_phase).numpy())
-    print("- train_reference_class_short tensor shape: ", tf.shape(train_reference_class_short).numpy())
-    print("- test_reference_class_short tensor shape: ", tf.shape(test_reference_class_short).numpy())
+    logging.info(f"- train_measured_phase tensor shape: {tf.shape(train_measured_phase).numpy()}")
+    logging.info(f"- test_measured_phase tensor shape: {tf.shape(test_measured_phase).numpy()}")
+    logging.info(f"- train_reference_class_short tensor shape: {tf.shape(train_reference_class_short).numpy()}")
+    logging.info(f"- test_reference_class_short tensor shape: {tf.shape(test_reference_class_short).numpy()}")
 
     # Normalize "pixel" values to be between 0 and 1
     train_measured_phase = (train_measured_phase - np.min(train_measured_phase)) / (np.max(train_measured_phase) - np.min(train_measured_phase))
@@ -135,7 +139,7 @@ for nRUN in range(30, 301, 10):
     
     # Evaluate the model: print the test accuracy
     test_loss, test_acc = model.evaluate(test_measured_phase, test_reference_class_short, verbose=2)
-    print("test accuracy: ", test_acc)
+    logging.info(f"test accuracy: {test_acc}")
 
     # Predict the classes
     predictions = model.predict(test_measured_phase)
@@ -143,7 +147,7 @@ for nRUN in range(30, 301, 10):
 
     # Print classification report
     report = classification_report(test_reference_class_short, predicted_classes, target_names=classes, digits=6, output_dict=False)
-    print(report)
+    logging.info(report)
 
     # Save classification report to file in Downloads folder
     downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
@@ -156,5 +160,3 @@ for nRUN in range(30, 301, 10):
     # Save plot to file in Downloads folder
     plt.savefig(os.path.join(downloads_folder, f'training_val_acc_{nRUN}nRUN_{x_resize}xres_{y_resize}yres.png'))
     plt.close()
-
-    
